afaf7e3092df9af2e8e1a31d1c1f1ee61377ed7e
Lucas Brito <lsbrito27@gmail.com>
2025-02-18 23:12:40 -0300

    bugfix test

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,11 @@ public class HelloWorld{
 
     public static void main(String[] args){
         System.out.println("Hello World!");
+      
+       String nome = "Teste";
+        if(nome.equals("Teste")){
+            System.out.println("Teste OK!");
+        }
     }
 
 }
